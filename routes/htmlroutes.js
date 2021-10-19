8bf99cdd64ef9a2b365497b592fb582ce1d0f862
NeoNeonilla <nelliafleurova@gmail.com>
2021-10-19 13:05:59 -0500

    cleaning up the html routes

--- routes/htmlroutes.js
+++ routes/htmlroutes.js
@@ -11,27 +11,5 @@ express.get('*', (req, res) => {
 
 }
 )
-// Setup the /api/notes post route
-// express.post("/api/notes", function(req, res) {
-//     // Receives a new note, adds it to db.json, then returns the new note
-//     let newNote = req.body;
-//     notes.push(newNote);
-//     updateDb();
-//     return console.log("Added new note: "+newNote.title);
-// });
-
-// // Retrieves a note with specific id
-// express.get("/api/notes/:id", function(req,res) {
-//     // display json for the notes array indices of the provided id
-//     res.json(notes[req.params.id]);
-// });
-
-// // Deletes a note with specific id
-// express.delete("/api/notes/:id", function(req, res) {
-//     notes.splice(req.params.id, 1);
-//     updateDb();
-//     console.log("Deleted note with id "+req.params.id);
-// });
-
 
 module.exports = express;
